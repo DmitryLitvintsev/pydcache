ddee062c61b5374cb425a50b3757cfe264ad065e
Dmitry Litvintsev <litvinse@fnal.gov>
2026-04-09 16:34:39 -0500

    prune unused imports

--- pydcache/util/admin.py
+++ pydcache/util/admin.py
@@ -1,26 +1,11 @@
 from __future__ import annotations
 
-import argparse
-import errno
 import logging
-import multiprocessing
-import os
 import re
-import socket
-import subprocess
-import sys
-import time
-import uuid
-from multiprocessing import Lock, Process, Queue
-from pathlib import Path
 from typing import Any, Dict, List, Optional, Tuple, NoReturn
 from urllib.parse import urlparse
 
 import paramiko
-import psycopg2
-import psycopg2.extensions
-import psycopg2.extras
-import yaml
 
 logger = logging.getLogger(__name__)
 
